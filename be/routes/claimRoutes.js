0468bd20b435d133790ff08cab61505f2478d298
sowmyaG-2002 <84969839+sowmyaG-2002@users.noreply.github.com>
2025-07-18 14:55:49 +0530

    Delete be directory

--- be/routes/claimRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { claimPoints } = require('../controllers/claimController');
-
-router.post('/', claimPoints);
-
-module.exports = router;
